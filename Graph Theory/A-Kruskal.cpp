160c3d410b265ac2019d4019277f4ba501ae2805
Louis Lee <42908892+CrackLewis@users.noreply.github.com>
2022-08-11 04:44:51 +0000

    add kruskal

--- Graph Theory/A-Kruskal.cpp
+++ Graph Theory/A-Kruskal.cpp
@@ -1,20 +1,40 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 // Implementation based on adjacency linklist.
 template<typename WeTy=int>
 struct graph1
 {
-  vector<int> head,to,nxt;
-  vector<WeTy> we;
-  int n,eid;
-  graph1(int _n):n(_n),head(_n+1,-1),eid(-1){}
-  void addedge(int u,int v,WeTy w=WeTy())
+  /* Refer to @-Graph Definition */
+
+  // Minimum spanning tree - Kruskal Algorithm
+  // Tell if an MST exists by checking if krus_ans==n-1.
+  WeTy krus_wesum; int krus_ans;
+  using KEDGE=tuple<int,int,WeTy>;
+  graph1<WeTy> mst_kruskal()
   {
-    assert((u<=n&&v<=n)&&"vertex out of range");
-    to.emplace_back(v);
-    nxt.emplace_back(head[u]);
-    we.emplace_back(w);
-    head[u]=++eid;
+    //union-find set
+    vector<int> fa(n+1);
+    for(int i=1;i<=n;++i) fa[i]=i;
+    function<int(int)> find=[&](int x){return fa[x]==x?x:(fa[x]=find(fa[x]));};
+    auto merge=[&](int x,int y){x=find(x),y=find(y);if(x!=y)fa[x]=y;};
+    
+    auto edgecomp=[](const KEDGE& lhs,const KEDGE& rhs){return get<2>(lhs)<get<2>(rhs); };
+    vector<KEDGE> krue;
+    //convert edges to sortable edges
+    for(int i=1;i<=n;++i)
+      for(int j=head[i];~j;j=nxt[j]) krue.emplace_back(make_tuple(i,to[j],we[j]));
+    sort(krue.begin(),krue.end(),edgecomp);
+
+    graph1<WeTy> ans(n); int u,v; WeTy w;
+    krus_wesum=WeTy(),krus_ans=0;
+    for(auto& e:krue)
+    {
+      u=get<0>(e),v=get<1>(e),w=get<2>(e);
+      if(find(u)==find(v)) continue;
+      merge(u,v);
+      krus_ans++,krus_wesum+=w;
+      // It's assumed that exporting graph is undirected.
+      // Might need modification in specific problems.
+      ans.addedge(u,v,w),ans.addedge(v,u,w);
+    }
+    return ans;
   }
 };
