922697de17dd123556f042f2f948107b201b4ba5
Louis Lee <42908892+CrackLewis@users.noreply.github.com>
2022-10-09 06:11:49 +0000

    add bipartite dyeing

--- Data Structure/B-DSU.cpp
+++ Data Structure/B-DSU.cpp
@@ -4,6 +4,7 @@ using namespace std;
 /**
  * Disjoint Set Union
  *
+ * A tricky but useful structure in various problems.
  */
 
 struct DSU {
@@ -16,5 +17,35 @@ struct DSU {
   void merge(int x, int y) {
     x = find(x), y = find(y);
     if (x != y) fa[y] = x;
+  }
+};
+
+/**
+ * Bipartite Dyeing Problem in DSU Method
+ *
+ * Input: all edges in the bipartite graph (w=0:same side,w=1:opposed)
+ * Output: dyeing result or report a conflict
+ */
+struct BipartiteDyeingDSU : public DSU {
+  int n;
+
+  BipartiteDyeingDSU(int _n, const vector<tuple<int, int, int>>& edges)
+      : n(_n), DSU(2 * _n) {
+    // this part uses C++17 grammar
+    for (auto [u, v, we] : edges) {
+      if (we)
+        merge(u, v + n), merge(u + n, v);
+      else
+        merge(u, v), merge(u + n, v + n);
+    }
+  }
+
+  int result(vector<int>& res) {
+    for (int i = 1; i <= n; ++i)
+      if (find(i) == find(i + n)) return 0;
+    res.resize(n + 1, 0);
+    // same sided with 1 is named side 0
+    for (int i = 1; i <= n; ++i) res[i] = !(find(i) == find(1));
+    return 1;
   }
 };
